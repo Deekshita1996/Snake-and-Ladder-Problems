25c4ea9df989aca50c4811c7b322f23badd6eba2
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-18 21:47:40 +0530

    Player rolls dice to get number
    Player rolls dice to get number

---
+++ UseCase2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    internal class UseCase2
+    {
+        public static void RollDice()
+        {
+            int START_POSITION = 0;
+            int END_POSITION = 100;
+            int dieRolling;
+
+            Random random = new Random();
+            dieRolling = random.Next(1, 7);
+            Console.WriteLine("Number on die is: " + dieRolling);
+        }
+    }
+}
